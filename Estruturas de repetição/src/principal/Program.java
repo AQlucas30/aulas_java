82bf4a42f4ee0924cc8a22beb032f0de870f0b0a
Lucas <qlucas06@gmail.com>
2023-05-18 21:47:45 -0300

    quinta

---
+++ Estruturas de repetição/src/principal/Program.java
@@ -0,0 +1,23 @@
+package principal;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//mais utilizada quando já sabemos a quantidade de vezes que precisamos repetir os comandos
+		
+		// sintaxe do for
+		// 1ºvariavel que inicia o contador
+		// 2º condição a ser aceita;
+		// 3º incremento
+		
+	
+		
+		
+		for (int contador = 0; contador < 10; contador++) {
+		System.out.println("valor do contador: " + contador);
+	}
+
+	}
+}
