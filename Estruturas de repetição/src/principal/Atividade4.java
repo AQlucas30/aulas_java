f52dc805578f07980d19dabdbfae4de8cf45eba6
Lucas <qlucas06@gmail.com>
2023-05-22 21:47:47 -0300

    alteracao 22/05 - 2

---
+++ Estruturas de repetição/src/principal/Atividade4.java
@@ -0,0 +1,43 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Atividade4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner (System.in);
+	
+		int i;
+	
+		
+		System.out.println("Qual operação irá ser feita: soma, subtração, multiplicação ou divisão ");
+		String operacao = new  sc.nextLine();
+		
+		System.out.println("Digite o numero da tabuada: ");
+		String numero = sc.nextLine();
+		
+		System.out.println("Tabuada do " + numero + " : ");
+		
+		
+		for (i = 1; 1 <= 10; i++ ) {
+			int resultado;
+		
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
