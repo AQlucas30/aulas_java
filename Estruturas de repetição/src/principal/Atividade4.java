2f764a39db7e146455bdb4a26a47395890c844cd
Lucas <qlucas06@gmail.com>
2023-05-23 21:32:49 -0300

    alteração 23/05

--- Estruturas de repetição/src/principal/Atividade4.java
+++ Estruturas de repetição/src/principal/Atividade4.java
@@ -10,34 +10,45 @@ public class Atividade4 {
 		Scanner sc = new Scanner (System.in);
 	
 		int i;
+		double d;
 	
 		
-		System.out.println("Qual operação irá ser feita: soma, subtração, multiplicação ou divisão ");
-		String operacao = new  sc.nextLine();
+		System.out.println("Qual operação irá ser feita: +, -, * ou / ");
+		char operacao = sc.next().charAt(0);
 		
 		System.out.println("Digite o numero da tabuada: ");
-		String numero = sc.nextLine();
-		
-		System.out.println("Tabuada do " + numero + " : ");
+		int numero = sc.nextInt();
 		
 		
-		for (i = 1; 1 <= 10; i++ ) {
-			int resultado;
-		
+		switch (operacao) {
+		case '+':
+		    for (i = 1; i <= 100; i++ ) {
+			System.out.println(numero + " + " + i + " = " + (i + numero));		
+		}
+			break; 
+		case '-':	
+		    for (i = 1; i <= 10; i++ )	{
+			System.out.println(numero + " - " + i + " = " + (numero - i));
+		}
+		    break;
+		case '*':
+			for (i = 1; i <=10; i++) {	
+				System.out.println(numero + " * " + i + " = " + (i * numero));
+		}
+			break;
+		case '/':	
+		    for (d = 1; d <=10; d++) {	
+				System.out.println(numero + " / " + d + " = " + (d / numero));
+		}
+			break; 
+			
+			default: 	
+	
+			System.out.println("opçao não encontrad!!a");
 			
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		sc.close();
 
-	}
-
+		
+		}
 }
