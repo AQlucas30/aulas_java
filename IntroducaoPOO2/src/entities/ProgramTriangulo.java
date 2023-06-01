c39fa9629a8cff4b330d2b51e73ae104cbd10a2b
Lucas <qlucas06@gmail.com>
2023-06-01 19:36:27 -0300

    01/06 primeira

--- IntroducaoPOO2/src/entities/ProgramTriangulo.java
+++ IntroducaoPOO2/src/entities/ProgramTriangulo.java
@@ -1,5 +1,14 @@
 package entities;
 
+import principal.Triangulo;
+
 public class ProgramTriangulo {
+	
+	public double triangulo;
+	
+	public void origemTriangulo(double lado1, double lado2, double lado3) {
+		
+	}
+	
 
 }
--- IntroducaoPOO2/src/principal/Triangulo.java
+++ IntroducaoPOO2/src/principal/Triangulo.java
@@ -1,9 +1,36 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Triangulo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner (System.in);
+		
+		Triangulo triangulo = new Triangulo();
+		
+		System.out.println("Qual valor do priemeiro lado: ");
+		double lado1 = sc.nextDouble();
+		
+		System.out.println("Qual o valor do segundo lado: ");
+		double lado2 = sc.nextDouble();
+		
+		System.out.println("Qual o valor do terceiro lado: ");
+		double lado3 = sc.nextDouble();
+		
+		if (lado1 == lado2 && lado2 == lado3) {
+			System.out.println("O triângulo é equilátero.");
+		}else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+			System.out.println("O triângulo é Isósceles.");
+		}else {
+			System.out.println("O triângulo é escaleno.");
+		}
+		
+		
+		sc.close();
+		
 
 	}
 
